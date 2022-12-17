8de5824d582383ab55e13e300656150039c8c2bb
warrenwongcodes <warren.wong4@gmail.com>
2022-12-17 12:29:38 -0500

    Feat: server file

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const db = require('./database/index.js');
+const app = express();
+const port = 4000;
+const router = require('./routes/index.js');
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static(__dirname + '/../public'));
+
+app.use('/', router);
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
